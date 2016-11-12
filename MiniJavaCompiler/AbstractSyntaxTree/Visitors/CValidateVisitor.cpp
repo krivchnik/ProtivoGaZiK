4676e83b02c50ffe0adad303611135d6e3a51922
ZatsepinMikhail <salamence1996@gmail.com>
2016-11-12 20:57:09 +0300

    Started to fill methods in CValidateVisitor

--- MiniJavaCompiler/AbstractSyntaxTree/Visitors/CValidateVisitor.cpp
+++ MiniJavaCompiler/AbstractSyntaxTree/Visitors/CValidateVisitor.cpp
@@ -10,32 +10,54 @@ void CValidateVisitor::StartVisit(INode *startNode, std::string filename) {
 
 }
 
-void CValidateVisitor::Visit(CAssignStatement *) {
+void CValidateVisitor::Visit(CAssignStatement *statement) {
 
+    statement->GetVariable()->Accept(this);
+    statement->GetExpression()->Accept(this);
 }
 
-void CValidateVisitor::Visit(CAssignItemStatement *) {
+void CValidateVisitor::Visit(CAssignItemStatement *statement) {
 
+    statement->getId()->Accept(this);
+    statement->getExpressionInBrackets()->Accept(this);
+    statement->getAssignedExpression()->Accept(this);
 }
 
-void CValidateVisitor::Visit(CPrintStatement *) {
-
+void CValidateVisitor::Visit(CPrintStatement *statement) {
+    statement->GetExpression()->Accept(this);
 }
 
-void CValidateVisitor::Visit(CIfElseStatement *) {
+void CValidateVisitor::Visit(CIfElseStatement *statement) {
 
+    statement->getExpression()->Accept(this);
+    statement->getIfStatement()->Accept(this);
+    statement->getElseStatement()->Accept(this);
 }
 
-void CValidateVisitor::Visit(CWhileStatement *) {
+void CValidateVisitor::Visit(CWhileStatement *statement) {
 
+    statement->getCondition()->Accept(this);
+    statement->getBody()->Accept(this);
 }
 
-void CValidateVisitor::Visit(CListStatement *) {
+void CValidateVisitor::Visit(CListStatement *statement) {
 
+    auto statements = statement->GetStatements();
+    size_t numberOfIterations = statements.size();
+
+    for(size_t i = 0; i < numberOfIterations; ++i) {
+        statements[i]->Accept(this);
+    }
 }
 
-void CValidateVisitor::Visit(CListExpression *) {
+void CValidateVisitor::Visit(CListExpression *expression) {
 
+    auto expressions = expression->GetExpressions();
+    size_t numberOfIterations = expressions.size();
+
+    for(size_t i = 0; i < numberOfIterations; ++i) {
+        expressions[i]->Accept(this);
+    }
 }
 
 void CValidateVisitor::Visit(CVarDecl *) {
@@ -74,16 +96,19 @@ void CValidateVisitor::Visit(CNotExpression *) {
 
 }
 
-void CValidateVisitor::Visit(COperationExpression *) {
+void CValidateVisitor::Visit(COperationExpression *expression) {
 
+    expression->GetLeftOperand()->Accept(this);
+    expression->GetRightOperand()->Accept(this);
 }
 
 void CValidateVisitor::Visit(CLengthExpression *) {
 
 }
 
-void CValidateVisitor::Visit(CArrayConstructionExpression *) {
+void CValidateVisitor::Visit(CArrayConstructionExpression *expression) {
 
+    expression->getSize()->Accept(this);
 }
 
 void CValidateVisitor::Visit(CConstructClassExpression *) {
