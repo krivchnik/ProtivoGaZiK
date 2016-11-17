788ff1db922771ea3c3eda11de3a3537cd777523
UPML <kagudkov@gmail.com>
2016-11-17 19:47:13 +0300

    Создал визитор для проверки типов

--- MiniJavaCompiler/AbstractSyntaxTree/Visitors/CCheckTypesVisitor.cpp
+++ MiniJavaCompiler/AbstractSyntaxTree/Visitors/CCheckTypesVisitor.cpp
@@ -1,2 +1,138 @@
 
 #include <Visitors/CCheckTypesVisitor.h>
+#include <CommonInclude.h>
+
+
+void CCheckTypesVisitor::Visit(COperationExpression* expression) {
+
+    expression->GetLeftOperand()->Accept(this);
+    expression->GetRightOperand()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit(CNumExpression* expression) {
+}
+
+void CCheckTypesVisitor::Visit(CBoolExpression* expression) {
+}
+
+void CCheckTypesVisitor::Visit(CThisExpression* expression) {
+}
+
+void CCheckTypesVisitor::Visit(CIdExpression* expression) {
+}
+
+void CCheckTypesVisitor::Visit(CNotExpression* expression) {
+    expression->GetExpression()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit(CLengthExpression* expression) {
+    expression->getExpression()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit(CArrayConstructionExpression* expression) {
+    expression->getSize()->Accept(this);
+
+}
+
+
+void CCheckTypesVisitor::Visit(CConstructClassExpression* expression) {
+}
+
+void CCheckTypesVisitor::Visit(CAssignStatement* statement) {
+    statement->GetVariable()->Accept(this);
+    statement->GetExpression()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit(CAssignItemStatement* statement) {
+    statement->getId()->Accept(this);
+    statement->getExpressionInBrackets()->Accept(this);
+    statement->getAssignedExpression()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit(CPrintStatement* statement) {
+    statement->GetExpression()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit(CIfElseStatement* statement) {
+    statement->getExpression()->Accept(this);
+    statement->getIfStatement()->Accept(this);
+    statement->getElseStatement()->Accept(this);
+}
+
+
+void CCheckTypesVisitor::Visit(CWhileStatement* statement) {
+    statement->getCondition()->Accept(this);
+    statement->getBody()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit(CListStatement* statement) {
+    auto statements = statement->GetStatements();
+    size_t numberOfIterations = statements.size();
+//    std::cout << "start analyze list of " << numberOfIterations << std::endl;
+
+    for(size_t i = 0; i < numberOfIterations; ++i) {
+        statements[i]->Accept(this);
+    }
+}
+
+void CCheckTypesVisitor::Visit(CListExpression* statement) {
+
+    auto statements = statement->GetExpressions();
+    size_t numberOfIterations = statements.size();
+
+    for(size_t i = 0; i < numberOfIterations; ++i) {
+        statements[i]->Accept(this);
+    }
+}
+
+void CCheckTypesVisitor::Visit(CVarDecl* decl) {
+}
+
+void CCheckTypesVisitor::Visit(CGetItemExpression* expression) {
+
+    expression->GetObject()->Accept(this);
+    expression->GetIndex()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit( CMethod* statement ) {
+
+    statement->getId()->Accept(this);
+    statement->getParameters()->Accept(this);
+    statement->getListDeclarations()->Accept(this);
+    statement->getListStatements()->Accept(this);
+    statement->getReturnExpression()->Accept(this);
+}
+
+
+void CCheckTypesVisitor::Visit( CMethodCallExpression* exp) {
+    exp->getObject()->Accept(this);
+    exp->getMethodId()->Accept(this);
+    exp->getArguments()->Accept(this);
+}
+
+void CCheckTypesVisitor::Visit( CClass* statement ) {
+    if (statement->getBaseId().get() != nullptr) {
+        statement->getBaseId()->Accept(this);
+    }
+    statement->getFields()->Accept(this);
+    statement->getMethods()->Accept(this);
+}
+
+
+void CCheckTypesVisitor::Visit( CMainClass* statement ) {
+    statement->GetClassId()->Accept(this);
+    statement->GetArgId()->Accept(this);
+    statement->GetStatement()->Accept(this);
+}
+
+
+void CCheckTypesVisitor::Visit(CProgram *statement) {
+
+    statement->GetMainClass()->Accept(this);
+    statement->GetMinorClasses()->Accept(this);
+}
+
+CCheckTypesVisitor::CCheckTypesVisitor(std::map<std::string, ClassInfo> &_classes) {
+    classes = _classes;
+}
+
--- MiniJavaCompiler/AbstractSyntaxTree/Visitors/CCheckTypesVisitor.h
+++ MiniJavaCompiler/AbstractSyntaxTree/Visitors/CCheckTypesVisitor.h
@@ -1,8 +1,54 @@
 #pragma once
 
 #include <Visitors/IVisitor.h>
-class CCheckTypesVisitor : IVisitor {
+#include <Nodes/CClass.h>
+#include <Visitors/CInfoClasses.h>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <ostream>
 
+
+
+class CCheckTypesVisitor : public IVisitor {
+
+public:
+
+    CCheckTypesVisitor(std::map<std::string, ClassInfo >& _classes);
+
+    //если увидим ещё раз класс с тем же именем, то препишем его уже существующему CIdExpression;
+    void Visit(CAssignStatement*);
+    void Visit(CAssignItemStatement*);
+    void Visit(CPrintStatement*);
+    void Visit(CIfElseStatement*);
+    void Visit(CWhileStatement*);
+
+    void Visit(CListStatement*);
+    void Visit(CListExpression*);
+
+    void Visit(CVarDecl*);
+    void Visit(CMethod*);
+    void Visit(CClass*);
+    void Visit(CMainClass*);
+    void Visit(CProgram*);
+
+    void Visit(CIdExpression*);
+    void Visit(CNumExpression*);
+    void Visit(CBoolExpression*);
+    void Visit(CNotExpression*);
+    void Visit(COperationExpression*);
+    void Visit(CLengthExpression*);
+    void Visit(CArrayConstructionExpression*);
+    void Visit(CConstructClassExpression*);
+    void Visit(CMethodCallExpression*);
+    void Visit(CThisExpression*);
+    void Visit(CGetItemExpression*);
+
+private:
+
+    std::map< std::string, ClassInfo > classes;
 
 };
+
 
